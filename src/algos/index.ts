05a400759004974888c531f3cf6796e6d954ec68
meatswitch <66923660+meatswitch@users.noreply.github.com>
2023-07-30 18:39:54 -0700

    Update index.ts
    changed all instances of whats-alf or whatsAlf to uncut-feed or uncutFeed, respectively

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as uncutFeed from './uncut-feed'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [uncutFeed.shortname]: uncutFeed.handler,
 }
 
 export default algos
